588cee161009ca389def94382837f4bac9a3fcae
Design Leaders <designleaders.bd@gmail.com>
2022-04-22 15:41:32 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dice App'),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        backgroundColor: Colors.red,
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftButtonPressed = 1;
+  int rightButtonPressed = 1;
+  void changeDice() {
+    setState(() {
+      leftButtonPressed = Random().nextInt(6) + 1;
+      rightButtonPressed = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$leftButtonPressed.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$rightButtonPressed.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
